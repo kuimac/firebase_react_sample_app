3fea79a8d7a3fcbcde43f61334acc085d52ac344
Kousuke Kumagai <kumagaikousuke1995@gmail.com>
2023-10-10 02:57:14 +0900

    ドキュメントが存在しなかった場合にだけ新規に作成するようにした

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -3,21 +3,28 @@ import { useNavigate, Navigate, Link } from "react-router-dom"
 import type { User } from "@firebase/auth"
 import { onAuthStateChanged, signOut } from "firebase/auth"
 import { fireAuth, firebaseApps } from "../FirebaseConfig"
-import { collection, doc, setDoc, serverTimestamp } from "firebase/firestore"
+import { collection, doc, setDoc, serverTimestamp, getDoc, DocumentData } from "firebase/firestore"
+
+export type userType = {
+  display_name: string
+  created_at: Date
+}
 
 const MyPage = () => {
-  const [user, setUser] = useState<User>()
+  const [authUserData, setAuthUserData] = useState<User>()
+  const [userData, setUserData] = useState<DocumentData>()
   const [isLoading, setIsLoading] = useState(true)
   const [isLogin, setIsLogin] = useState<boolean>(false)
 
   /**
    * Firebaseでログインしているかを判断する
-   * ログインしている場合はuser情報をstateに格納する
+   * ログインしている場合はauthUser情報をstateに格納する
    */
   useEffect(() => {
     onAuthStateChanged(fireAuth, (currentUser) => {
       if (!currentUser) return
-      setUser(currentUser)
+      // TODO: currentUser.uid まで格納されていてセキュリティ的によろしくないので使用するものだけ格納する
+      setAuthUserData(currentUser)
       setIsLogin(true)
       setIsLoading(false)
     })
@@ -25,16 +32,26 @@ const MyPage = () => {
 
   /**
    *  Firestore にユーザー用のドキュメントが作られていなければ作成する
+   *  存在する場合はstateにuserデータを入れる
    */
   useLayoutEffect(() => {
     fireAuth.onAuthStateChanged(async (currentUser) => {
       if (!currentUser) return
       const usersDocRef = collection(firebaseApps.db, "users")
-      await setDoc(doc(usersDocRef, currentUser.uid), {
-        // name: currentUser.uid,
-        display_name: "",
-        created_at: serverTimestamp()
-      })
+      const userDocumentRef = doc(firebaseApps.db, "users", currentUser.uid)
+
+      if (!userDocumentRef) {
+        await setDoc(doc(usersDocRef, currentUser.uid), {
+          // name: currentUser.uid,
+          display_name: "aa",
+          created_at: serverTimestamp()
+        })
+      } else {
+        getDoc(userDocumentRef).then((documentSnapshot) => {
+          if (!documentSnapshot) return
+          setUserData(documentSnapshot.data())
+        })
+      }
     })
   }, [])
 
@@ -43,6 +60,9 @@ const MyPage = () => {
     await signOut(fireAuth)
     navigate("/login/")
   }
+  const dateText = `${userData?.created_at.toDate().getFullYear()}年${
+    userData?.created_at.toDate().getMonth() + 1
+  }月${userData?.created_at.toDate().getDate()}日`
 
   return (
     <>
@@ -52,9 +72,15 @@ const MyPage = () => {
             <Navigate to={`/login/`} />
           ) : (
             <>
-              <h1>マイページ</h1>
-              <p>{user?.email}</p>
-              <button onClick={logout}>ログアウト</button>
+              <>
+                <h1>マイページ</h1>
+                <p>{authUserData?.email}</p>
+                <button onClick={logout}>ログアウト</button>
+              </>
+              <>
+                <p>{userData?.display_name}</p>
+                <p>{`作成日 ${dateText}`}</p>
+              </>
             </>
           )}
         </>
